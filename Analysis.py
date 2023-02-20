ef3221808d18422eac0ea7ea04744cdd9928d0f1
Àlex Giménez Romero <gimenez.romero.alex@gmail.com>
2023-02-20 14:59:24 +0100

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -12,7 +6,16 @@ import pygeos
 from rtree import index
 from shapely.strtree import STRtree
 
+from osgeo import gdal
+
+gdal.SetConfigOption('OGR_GEOJSON_MAX_OBJ_SIZE', '2000MB')
+
 import powerlaw
+
+import os
+import time
+
+import matplotlib.pyplot as plt
 
 def power_law(x, p, A):
     
@@ -34,11 +37,37 @@ def truncated_power_law_norm(x, α, λ, xmin):
     
     return np.power(x, -α) * np.exp(-λ * x) * C
 
+def reef_size_distribution_txt(input_folder, output_folder_pdf, output_folder_ccdf):
 
-# In[ ]:
+    filenames = os.listdir(input_folder)
 
+    for filename in filenames:
 
-def compute_size_distribution(input_folder, output_filename, plot=False, figname="Figures/size_dist"):
+        t0 = time.time()
+
+        name = filename[18:-8]
+
+        print("Reading %s..." % name, end="")
+
+        gdf = gpd.read_file("%s/%s" % (input_folder, filename))
+
+        print("computing powerlaw...", end="")
+
+        cluster_sizes = gdf.to_crs(epsg="3035").area #gdf["area (m2)"].values
+
+        results = powerlaw.Fit(cluster_sizes, xmin=np.amin(cluster_sizes))
+
+        bins_pdf, values_pdf = results.pdf()
+        bins_ccdf, values_ccdf = results.ccdf()
+
+        center_bins_pdf = bins_pdf[0:-1] + (bins_pdf - np.roll(bins_pdf, shift=1))[1:] / 2
+
+        np.savetxt("%s/%s.txt" % (output_folder_pdf, name), np.transpose([center_bins_pdf, values_pdf]))
+        np.savetxt("%s/%s.txt" % (output_folder_ccdf, name), np.transpose([bins_ccdf, values_ccdf]))
+
+        print("done in %.2f m" % ((time.time()-t0)/60.0))
+
+def compute_size_distribution(input_folder, output_filename, plot=False, figname="Figures/"):
     
     filenames = os.listdir(input_folder)
 
@@ -47,6 +76,11 @@ def compute_size_distribution(input_folder, output_filename, plot=False, figname
     optimal_Ds = []
 
     names = []
+
+    if not os.path.exists(figname + "PDF/"): os.mkdir(figname + "PDF/")
+    if not os.path.exists(figname + "CCDF/"): os.mkdir(figname + "CCDF/")
+    if not os.path.exists(figname + "PDF_whole_range/"): os.mkdir(figname + "PDF_whole_range/")
+    if not os.path.exists(figname + "CCDF_whole_range/"): os.mkdir(figname + "CCDF_whole_range/")
 
     for filename in filenames:
 
@@ -58,14 +92,15 @@ def compute_size_distribution(input_folder, output_filename, plot=False, figname
 
         print("computing powerlaw...", end="")
 
-        cluster_sizes = gdf["area (m2)"].values
+        cluster_sizes = gdf.to_crs(epsg="3035").area #gdf["area (m2)"].values
 
         results = powerlaw.Fit(cluster_sizes, xmin=np.amin(cluster_sizes))
 
-        plt.figure(figsize=(8, 6))
+        fig1, ax1 = plt.subplots(figsize=(8, 6))
+        fig2, ax2 = plt.subplots(figsize=(8, 6))
 
-        fig = results.plot_pdf(ls='', marker='o', color='k', markerfacecolor='w', markersize=15, 
-                               label="")
+        results.plot_pdf(ax=ax1, ls='', marker='o', color='k', markerfacecolor='w', markersize=15, label="")
+        results.plot_ccdf(ax=ax2, ls='', marker='o', color='k', markerfacecolor='w', markersize=15, label="")
 
         #Get approx xmin:
         Ds = []
@@ -96,34 +131,95 @@ def compute_size_distribution(input_folder, output_filename, plot=False, figname
         optimal_Ds.append(np.amin(Ds))
         names.append(name)
 
-        x = [10**i for i in range(len(str(int(np.amin(cluster_sizes)))), 
-                                  len(str(int(np.amax(cluster_sizes))))+1)]
+        x = np.array([10**i for i in range(len(str(int(np.amin(cluster_sizes)))), 
+                                  len(str(int(np.amax(cluster_sizes))))+1)])
 
         if plot == True:
         
-            plt.plot(x, power_law_norm(x, alpha, xmin), lw=3, color='r', 
-                     label=r"$y\sim x^{\alpha}, \alpha=%.2f$" % alpha)
+            #Plot PDF whole range
+            ax1.plot(x, power_law_norm(x, alpha, xmin), lw=3, color='r', 
+                        label=r"$y\sim x^{-\alpha}, \alpha=%.2f$" % alpha)
 
-            plt.xticks(fontsize=16)
-            plt.yticks(fontsize=16)
+            ax1.tick_params(axis='both', which='major', labelsize=16)
 
-            plt.xlabel(r"$Km^2$", fontsize=30)
-            plt.ylabel("PDF", fontsize=30)
+            ax1.set_xlabel(r"$Km^2$", fontsize=30)
+            ax1.set_ylabel("PDF", fontsize=30)
 
-            plt.yscale("log")
-            plt.xscale("log")
+            ax1.set_yscale("log")
+            ax1.set_xscale("log")
 
-            plt.legend(loc="upper right", fontsize=20);
+            ax1.legend(loc="upper right", fontsize=20);
 
-            plt.savefig(figname + "_%s.png" % name, bbox_inches='tight', dpi=300)
+            fig1.savefig(figname + "PDF_whole_range/%s.png" % name, bbox_inches='tight', dpi=300)
 
-            plt.close()
+            #Plot CCDF whole range
+            ax2.plot(x, power_law_CCDF(x, alpha, xmin), lw=3, color='r', 
+                        label=r"$y\sim x^{1-\alpha}, \alpha=%.2f$" % alpha)
+
+            ax2.tick_params(axis='both', which='major', labelsize=16)
+
+            ax2.set_xlabel(r"$Km^2$", fontsize=30)
+            ax2.set_ylabel("CCDF", fontsize=30)
+
+            ax2.set_yscale("log")
+            ax2.set_xscale("log")
+
+            ax2.legend(loc="upper right", fontsize=20);
+
+            fig2.savefig(figname + "CCDF_whole_range/%s.png" % name, bbox_inches='tight', dpi=300)
+
+            #Plot from xmin
+            x = np.array([10**i for i in range(len(str(int(xmin)))-1, 
+                                    len(str(int(np.amax(cluster_sizes))))+1)])
+
+            #PDF
+            fig3, ax3 = plt.subplots(figsize=(8, 6))
+
+            results.plot_pdf(ax=ax3, ls='', marker='o', color='k', markerfacecolor='w', markersize=15, label="")
+
+            ax3.plot(x, power_law_norm(x, alpha, xmin), lw=3, color='r', 
+                        label=r"$y\sim x^{-\alpha}, \alpha=%.2f$" % alpha)
+
+            ax3.tick_params(axis='both', which='major', labelsize=16)
+
+            ax3.set_xlabel(r"$Km^2$", fontsize=30)
+            ax3.set_ylabel("PDF", fontsize=30)
+
+            ax3.set_yscale("log")
+            ax3.set_xscale("log")
+
+            ax3.legend(loc="upper right", fontsize=20);
+
+            fig3.savefig(figname + "PDF/%s.png" % name, bbox_inches='tight', dpi=300)
+
+            #CCDF
+            fig4, ax4 = plt.subplots(figsize=(8, 6))
+
+            results.plot_ccdf(ax=ax4, ls='', marker='o', color='k', markerfacecolor='w', markersize=15, label="")
+            
+            ax4.plot(x, power_law_CCDF(x, alpha, xmin), lw=3, color='r', 
+                        label=r"$y\sim x^{1-\alpha}, \alpha=%.2f$" % alpha)
+
+            ax4.tick_params(axis='both', which='major', labelsize=16)
+
+            ax4.set_xlabel(r"$Km^2$", fontsize=30)
+            ax4.set_ylabel("CCDF", fontsize=30)
+
+            ax4.set_yscale("log")
+            ax4.set_xscale("log")
+
+            ax4.legend(loc="upper right", fontsize=20);
+
+            fig4.savefig(figname + "CCDF/%s.png" % name, bbox_inches='tight', dpi=300)
+            
+            plt.close("all")
+
 
         print("Done!")
 
     df = pd.DataFrame({'Name':names, "Alpha":optimal_alphas, "Xmin":optimal_xmins, "D":optimal_Ds})
 
-    df.to_csv(output_filename)
+    df.to_csv("%s.csv" % output_filename)
 
 def create_square_grid_gdf(lons, lats):
 
@@ -170,25 +266,16 @@ def create_square_grid_gdf(lons, lats):
     
     return mesh_gdf
 
-def box_counting_dimension(filename, epsilons):
+def box_counting_dimension(input_folder, filename, epsilons, output_folder):
+
+    if not os.path.exists(output_folder): os.mkdir(output_folder)
 
     #Load coral data
     print("Reading file...")
-    gdf = gpd.read_file("Coral_clusters/Coral_Rock/%s" % filename)
-
-    #Compute centroids (again...)
-    centroids = gdf["geometry"].to_crs(epsg="3035").centroid.to_crs(epsg="4326")
-
-    cntr_lon = [item.x for item in centroids]
-    cntr_lat = [item.y for item in centroids]
-
-    gdf["cntr_lon"] = cntr_lon
-    gdf["cntr_lat"] = cntr_lat
+    gdf = gpd.read_file("%s/%s" % (input_folder, filename))
 
     #Tree for corals geometry
     tree = STRtree(gdf["geometry"])
-
-    #Create index for Tree-labeled objects
 
     #Get maximum and minimum latlon values to create a mesh
     max_latlon = gdf["geometry"].bounds
@@ -202,9 +289,11 @@ def box_counting_dimension(filename, epsilons):
 
     N_boxes = []
 
+    print("ϵ\tN")
+
     for ϵ in epsilons:
         
-        print("Computing ϵ=%s" % ϵ)
+        #print("Computing ϵ=%s" % ϵ)
 
         #Create mesh with length ϵ
         x_c = np.arange(min_lon-ϵ, max_lon, ϵ)
@@ -218,24 +307,219 @@ def box_counting_dimension(filename, epsilons):
         mesh = create_square_grid_gdf(xx_v, yy_v)
 
         #Compute number of overlapping boxes
-        idxs = []
+        idxs = 0
 
         for i in range(len(mesh)):
 
+            #Indices of the polygons that intersect the box
             query = tree.query(mesh["geometry"][i])
 
-            for item in query:
+            #If some polygon intersect the box, add 1
+            if len(query) > 0:
 
-                if item.intersects(mesh["geometry"][i]):
+                idxs += 1
 
-                    idxs.append(i)
+        N_boxes.append(idxs)
 
-                    break
+        print(ε, idxs)
 
-        N_boxes.append(len(idxs))
-
-    np.savetxt("Box_Counting_Dimensions/" + filename[18:-8] + ".txt", np.transpose([epsilons, N_boxes]),
-               header="ϵ N")
+    np.savetxt("%s/%s.txt" % (output_folder, filename[0:-8]), np.transpose([epsilons, N_boxes]), header="ϵ N")
 
     print("\nFinished in ", time.time()-t0, "s")
 
+def Taylor_law(filename, ϵ):
+
+    mean_density = []
+    var_density = []
+
+    #Load coral data
+    print("Reading file...")
+    gdf = gpd.read_file("%s" % filename)
+
+    #Tree for corals geometry
+    tree = STRtree(gdf["geometry"])
+
+    #Get maximum and minimum latlon values to create a mesh
+    max_latlon = gdf["geometry"].bounds
+
+    max_lat = np.amax(max_latlon["maxy"])
+    min_lat = np.amin(max_latlon["miny"])
+    max_lon = np.amax(max_latlon["maxx"])
+    min_lon = np.amin(max_latlon["minx"])
+
+    print("Creating mesh...")
+
+    #Create mesh with length ϵ
+    x_c = np.arange(min_lon-ϵ, max_lon, ϵ)
+    y_c = np.arange(min_lat-ϵ, max_lat, ϵ)
+
+    x_v = np.append(x_c, x_c[-1] + ϵ) - ϵ/2
+    y_v = np.append(y_c, y_c[-1] + ϵ) - ϵ/2
+
+    yy_v, xx_v = np.meshgrid(y_v, x_v, indexing="ij") 
+
+    mesh = create_square_grid_gdf(xx_v, yy_v)
+
+    #Comput area of corals in each grid
+    print("Computing cell areas")
+
+    tot_its = len(mesh)
+
+    #Iterate through the cells of the mesh
+    for i in range(tot_its):
+
+        completed = (i / tot_its) * 100
+
+        if np.round(completed, 0) % 5 == 0:
+
+            print("%.0f%% completed" % completed)
+
+        #Obtain the inidces of the polygons that intersect the cell
+        query = tree.query(mesh["geometry"][i])
+
+        coral_areas = gdf["geometry"].iloc[query].intersection(mesh["geometry"][i]).to_crs(epsg="3035").area.values #In m²
+
+        #area_cell = mesh["geometry"].to_crs(epsg="3035").iloc[i].area #In m²
+
+        densities = coral_areas #/ area_cell
+
+        if len(densities) > 0:
+
+            mean_density.append(np.mean(densities))
+            var_density.append(np.var(densities))
+
+    return np.array(mean_density), np.array(var_density)
+
+def Area_Perimeter_scaling(input_folder, outfilename):
+
+    filenames = os.listdir(input_folder)
+
+    df_f = pd.DataFrame({"Province":[], "Area":[], "Perimeter":[]})
+
+    for filename in filenames:
+
+        t0 = time.time()
+        
+        print("Computing %s..." % filename[18:-8], end="")
+
+        gdf = gpd.read_file("Data/Coral/%s" % filename)
+
+        gdf = gdf.to_crs(epsg="3035")
+
+        df = pd.DataFrame({"Province":[filename[18:-8] for i in range(len(gdf))], "Area":gdf.area.values, "Perimeter":gdf.boundary.length.values})
+
+        df_f = pd.concat((df_f, df), ignore_index=True)
+
+        print("finished in %.2f m" % ((time.time() - t0) / 60.0))
+
+    df_f.to_parquet("%s.parquet" % outfilename, engine="fastparquet")
+
+def multifractal_dimension(qs, epsilons, filename, input_folder, output_folder):
+
+    for ε in epsilons:
+
+        t0 = time.time()
+
+        print("Computing ϵ=%.2e..." % ϵ, end="")
+
+        t0 = time.time()
+
+        gdf = gpd.read_file("%s/%s" % (input_folder, filename))
+
+        total_area = gdf["geometry"].to_crs(epsg="3035").area.sum()
+
+        #Tree for corals geometry
+        tree = STRtree(gdf["geometry"])
+
+        t0 = time.time()
+
+        #Get maximum and minimum latlon values to create a mesh
+        max_latlon = gdf["geometry"].bounds
+
+        max_lat = np.amax(max_latlon["maxy"])
+        min_lat = np.amin(max_latlon["miny"])
+        max_lon = np.amax(max_latlon["maxx"])
+        min_lon = np.amin(max_latlon["minx"])
+
+        #Create mesh with length ϵ
+        x_c = np.arange(min_lon-ϵ, max_lon, ϵ)
+        y_c = np.arange(min_lat-ϵ, max_lat, ϵ)
+
+        x_v = np.append(x_c, x_c[-1] + ϵ) - ϵ/2
+        y_v = np.append(y_c, y_c[-1] + ϵ) - ϵ/2
+
+        yy_v, xx_v = np.meshgrid(y_v, x_v, indexing="ij") 
+
+        mesh = create_square_grid_gdf(xx_v, yy_v)
+
+        #Mass measure
+        p_boxes = []
+
+        for i in range(len(mesh)):
+
+            #Indices of the polygons that intersect the box
+            query = tree.query(mesh["geometry"][i])
+
+            if len(query) > 0:
+
+                p_box = np.sum(gdf["geometry"].iloc[query].intersection(mesh["geometry"][i]).to_crs(epsg="3035").area.values) / total_area
+
+                if p_box > 0:
+
+                    p_boxes.append(p_box)
+
+        D_q = []
+        P_q = []
+
+        for q in qs:
+            
+            #Compute number of overlapping boxes
+            idxs = np.sum([np.power(p_box, q) for p_box in p_boxes]) 
+
+            D = (1/(q-1)) * (np.log(idxs) / np.log(ε))
+
+            D_q.append(D)
+            P_q.append(idxs)
+
+        print("done in %.2f m" % ((time.time() - t0) / 60.0))
+
+        np.savetxt("%s/%s_epsilon_%s.txt" % (output_folder, filename[0:-8], ε), np.transpose([qs, P_q, D_q]), header="q\tP_q\tD_q")
+
+def compute_province_boundaries(input_folder, outfilename):
+
+    filenames = os.listdir(input_folder)
+
+    min_lons = []
+    max_lons = []
+    min_lats = []
+    max_lats = []
+
+    provinces = []
+
+    for filename in filenames:
+
+        t0 = time.time()
+
+        print("Reading %s..." % filename[:-8], end="")
+
+        gdf = gpd.read_file("/data/bio/corals/Clusterized_data/Coral/%s" % filename)
+
+        max_latlon = gdf["geometry"].bounds
+
+        max_lat = np.amax(max_latlon["maxy"])
+        min_lat = np.amin(max_latlon["miny"])
+        max_lon = np.amax(max_latlon["maxx"])
+        min_lon = np.amin(max_latlon["minx"])
+
+        min_lons.append(min_lon)
+        max_lons.append(max_lon)
+        min_lats.append(min_lat)
+        max_lats.append(max_lat)
+
+        provinces.append(filename[:-8])
+
+        print("done in %.2f m" % ((time.time()-t0) / 60.0))
+
+    df = pd.DataFrame({'Province':provinces, 'Min lon':min_lons, 'Max lon':max_lons, 'Min lat':min_lats, 'Max lat':max_lats})
+
+    df.to_csv("%s.csv" % outfilename)
--- Preprocessing.py
+++ Preprocessing.py
@@ -2,11 +2,15 @@ import numpy as np
 import geopandas as gpd
 import os
 
+from osgeo import gdal
+
+gdal.SetConfigOption('OGR_GEOJSON_MAX_OBJ_SIZE', '2000MB')
+
 from shapely.strtree import STRtree
 
 def harmonize_data(input_folder, output_folder):
 
-    os.chdir(data_folder)
+    os.chdir(input_folder)
 
     filenames = os.listdir()
 
@@ -46,8 +50,68 @@ def harmonize_data(input_folder, output_folder):
         gdf_measures_final.to_file(output_folder + "/%s.geojson" % name, driver='GeoJSON')
 
         print("done!")
+
+def combine_polygons(name, input_folder, output_folder, class_save="Coral/Algae"):
+
+    if not os.path.exists(output_folder): 
         
-def combine_polygons(input_folder, output_folder, class_save="Coral/Algae"):
+        os.mkdir(output_folder)
+
+    #Read data
+    data = gpd.read_file("%s/%s.geojson" % (input_folder, name), driver="GeoJSON")
+    
+    gdf = data[data["class"] == class_save]
+
+    N_gdf = len(gdf)
+
+    #Compute tree
+    tree = STRtree(gdf["geometry"])
+
+    #index_by_id = dict((id(pt), i) for i, pt in enumerate(geometries)) -> This was needed with an older version of STRtree
+
+    labels = np.arange(0, N_gdf, 1)
+
+    new_label = N_gdf
+
+    #Create clusters
+    for i in range(len(gdf)):
+
+        intersections = tree.query(gdf["geometry"].iloc[i])
+
+        idx_intersect = []
+
+        if len(intersections) > 1:
+
+            for item in intersections:
+
+                if item == i: #gdf["geometry"][i] == item: -> This was needed with an older version of STRtree
+
+                    pass
+
+                elif gdf["geometry"].iloc[i].intersects(gdf["geometry"].iloc[item]): #gdf["geometry"][i].intersects(item): -> This was needed with an older version of STRtree
+
+                    #Index that intersect with polygon i                      
+                    idx_intersect.append(item) #idx_intersect.append(index_by_id[id(item)]) -> This was needed with an older version of STRtree
+
+        #Labels of the clusters that intersect cluster i
+        labels_idx_intersect = labels[idx_intersect]
+
+        #All clusters with same label get new label
+        for old_label in np.unique(labels_idx_intersect):
+
+            labels[labels == old_label] = new_label
+
+        #Cluster i gets new label
+        labels[i] = new_label
+
+        #Update new label
+        new_label += 1
+
+    combined_polygons = gdf.dissolve(by=labels, aggfunc="sum")
+
+    combined_polygons.to_file("%s/combined_polygons_%s.geojson" % (output_folder, name), driver="GeoJSON")
+        
+def combine_polygons_all(input_folder, output_folder, class_save="Coral/Algae"):
 
     if not os.path.exists(output_folder): 
         
